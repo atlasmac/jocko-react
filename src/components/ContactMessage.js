4fb7a8aeb3bf59cffcd10a904c45f5bf1d1ff7d8
atlasmac <mckinleyatlas@gmail.com>
2022-07-25 13:44:24 -0700

    add changes

--- src/components/ContactMessage.js
+++ src/components/ContactMessage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const ContactMessage = () => {
+
   return (
     <div>
       <h3 className='returnMessage'>Thanks for your message!<br /> We will be reaching out soon.</h3>
